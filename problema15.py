c352f2cfbae16369059a9a6e72a941db99080ac9
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-18 17:53:14 +0300

    Update problema15.py

--- problema15.py
+++ problema15.py
@@ -1,3 +1,5 @@
+Elevii clasei a V-a se repartizează în clase câte 25 în ordinea mediilor clasei a IV-a. Radu este pe locul x în ordinea mediilor. În
+ce clasa va fi repartizat (A, B, C, D sau E)?. Exemplu : date de intrare : x=73 date de ieşire : C.
 x=int(input("dati locul lui radu: "))
 if(x // 25 == 0):
     print("Radu va fi in clasa A")
