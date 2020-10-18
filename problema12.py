09d27df7c614de0e2463c4cd64bae52129245b23
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-18 17:50:33 +0300

    Add files via upload

---
+++ problema12.py
@@ -0,0 +1,11 @@
+a=int(input("dati numarul de petale ale unei flori: "))
+if (a % 5 == 0):
+    print("Nu ma iubeste de loc")
+    elif (a % 5 == 1):
+        print("Ma iubeste un pic")
+        elif (a % 5 == 2):
+            print("Ma iubeste mult")
+            elif (a % 5 == 3):
+                print("Ma iubeste cu pasiune")
+                elif (a % 5 == 4):
+                    print("Ma iubeste la nebunie")
