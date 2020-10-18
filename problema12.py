80e450054758a7a35eaf8453f81e0ea3d48f5355
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-18 17:50:57 +0300

    Update problema12.py

--- problema12.py
+++ problema12.py
@@ -1,3 +1,6 @@
+“Mă iubeşte un pic, mult, cu pasiune, la nebunie, de loc, un pic,…”. Rupând petalele unei margarete cu x petale, el (ea) mă
+iubeşte …. Exemplu: Date de intrare: x=10 Date de ieşire: … de loc.
+
 a=int(input("dati numarul de petale ale unei flori: "))
 if (a % 5 == 0):
     print("Nu ma iubeste de loc")
