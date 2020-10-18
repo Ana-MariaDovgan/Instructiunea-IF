aa40f914eb190788816f2ef8fd4238c9afe3dc8a
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-18 17:52:29 +0300

    Update problema14.py

--- problema14.py
+++ problema14.py
@@ -1,2 +1,4 @@
+Într-o tabără, băieţii sunt cazaţi câte 4 într-o căsuţă, în ordinea sosirii. Ionel a sosit al n-lea. În a câta căsuţă se va afla?
+Exemplu : date de intrare : n=69 date de ieşire : casuta 17.
 n=int(input("Al catilea a sosit Ionel: "))
 print("Ionel va fi cazat in casuta numarul",n // 4)
