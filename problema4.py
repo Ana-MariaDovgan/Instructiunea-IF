a8ecfc9050da570e0daf8b8fb44da6ed022ddd22
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-18 17:44:10 +0300

    Add files via upload

---
+++ problema4.py
@@ -0,0 +1,11 @@
+a=int(input("varsta primei persoane"))
+b=int(input("varsta a doua  persoana"))
+c=int(input("varsta a treia persoana"))
+if(a>18)and(a<60):
+    print(a)
+    if(b>18)and(b<60):
+        print(b)
+        if(c>18)and(c<60):
+            print(c)
+
+
