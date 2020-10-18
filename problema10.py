43d6ddaf7ec639a47306ed910249dfdb62d3f099
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-18 17:49:07 +0300

    Add files via upload

---
+++ problema10.py
@@ -0,0 +1,13 @@
+nrgaini=int(input("Numarul de gaini: "))
+nrboabe=int(input("Numarul de boabe: "))
+if(nrboabe % nrgaini==0):
+    print("Fiecare gaina a primit ",nrboabe // nrgaini," boabe iar curcanul nu a primit "):
+    elif(nrboabe % nrgaini !=0)
+    if((nrboabe % nrgaini)>(nrboabe // nrgaini)):
+        print("Curcanul a primit cu ",nrboabe % nrgaini,"boabe mai mult")
+        if((nrboabe % nrgaini)<(nrboabe // nrgaini)):
+            print("Fiecare gaina a primit cu ",nrboabe // nrgaini,"boabe mai mult")
+            else:
+                print("Numar egal")
+
+
