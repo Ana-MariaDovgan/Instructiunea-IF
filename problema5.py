cac05abd63e145b23b6e5647dcb10695b36498b0
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-18 17:45:14 +0300

    Update problema5.py

--- problema5.py
+++ problema5.py
@@ -1,3 +1,6 @@
+Cunoscând data curentă exprimată prin trei numere întregi reprezentând anul, luna, ziua precum şi data naşterii unei persoane,
+exprimată la fel, să se facă un program care să calculeze vârsta persoanei respective în număr de ani împliniţi. Exemplu : Date
+de intrare data curenta 2005 10 25 data nasterii 1960 11 2 Date de ieşre 44 ani.
 ac=int(input("Anul curent: "))
 lc=int(input("Luna curenta: "))
 zc=int(input("Ziua curenta: "))
