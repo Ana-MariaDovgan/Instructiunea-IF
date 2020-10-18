f25589ed81edb289d40390d074d6fd20efc4ace5
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-18 17:44:26 +0300

    Update problema4.py

--- problema4.py
+++ problema4.py
@@ -1,3 +1,5 @@
+Se introduc vârstele a 3 persoane. Afişaţi vârstele cuprinse între 18 şi 60 de ani. Exemplu : Date de intrare 56 34 12 Date de
+ieşire 56 34.
 a=int(input("varsta primei persoane"))
 b=int(input("varsta a doua  persoana"))
 c=int(input("varsta a treia persoana"))
