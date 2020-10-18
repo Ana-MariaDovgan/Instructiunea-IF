d5265d497c672ba777a4ec2a3a72e0900b228c94
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-18 17:51:38 +0300

    Update problema13.py

--- problema13.py
+++ problema13.py
@@ -1,3 +1,7 @@
+La un concurs se dau ca premii primilor 100 de concurenţi, tricouri de culoare albă, roşie, albastră şi neagră, în această
+secvenţă. Ionel este pe locul x. Ce culoare va avea tricoul pe care-l va primi? Exemplu : date de intrare : x=38 date de ieşire :
+rosie. 
+
 x= int(input("dati locul lui Ionel: "))
 if(x <= 100):
     if(x % 4 == 0):
