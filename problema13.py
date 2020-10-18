032c4188b15f8fb93952229ff23472772ecb9ea0
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-18 17:51:20 +0300

    Add files via upload

---
+++ problema13.py
@@ -0,0 +1,15 @@
+x= int(input("dati locul lui Ionel: "))
+if(x <= 100):
+    if(x % 4 == 0):
+        print("va primi un tricou negru")
+        elif(x % 4 == 1):
+            print("va primi un tricou alb")
+            elif(x % 4 == 2):
+                print("va primi un tricou rosu")
+                            elif(x % 4 == 3):
+                 print("va primi un tricou albastru")
+                 else:
+                     print("Ionel e intre primii 100 si nu va primi un premiu")
+
+
+
