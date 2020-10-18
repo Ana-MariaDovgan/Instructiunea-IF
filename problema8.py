4562b7b9f5e558d0e9638d682d0d200c9c3b92ea
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-18 17:47:35 +0300

    Update problema8.py

--- problema8.py
+++ problema8.py
@@ -1,3 +1,5 @@
+Să se afişeze cel mai mare număr par dintre doua numere introduse în calculator. Exemple : Date de intrare 23 45 Date de
+ieşire nu exista numar par ; Date de intrare 28 14 Date de ieşire 28 ; Date de intrare 77 4 Date de ieşire 4.
 n1=int(input("Primul numar: "))
 n2=int(input("Al doilea numar: "))
 if((n1%2==0)and (n2%2==0)):
