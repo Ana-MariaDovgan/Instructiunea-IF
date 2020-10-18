434847af007a2726bf48afda0ef7443b9be48598
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-10-18 17:49:47 +0300

    Add files via upload

---
+++ problema11.py
@@ -0,0 +1,15 @@
+n1=int(input("Primul numar: "))
+n2=int(input("Al doilea numar: "))
+n3=int(input("Al treilea numar: "))
+if (n1 % 2 == 0):
+    print(n1,"par")
+    else:
+        print(n1,"impar")
+        if(n2 % 2 == 0):
+            print(n2,"par")
+            else:
+                print(n2,"impar")
+                if(n3 % 2 == 0 ):
+                    print(n3,"par")
+                    else:
+                        print(n3,"impar")
